eabf7f94abaa8df1fcccd3be9d41666eb8f37f53
Aleksey <yowzamail@gmail.com>
2021-04-02 19:01:11 +0300

    add .xls tests

--- src/test/java/test/PdfTest.java
+++ src/test/java/test/PdfTest.java
@@ -2,13 +2,11 @@ package test;
 
 import com.codeborne.pdftest.PDF;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 
 import static com.codeborne.pdftest.PDF.containsText;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static utils.Files.getFile;
-
+import static utils.Files.getPdf;
 
 public class PdfTest {
 
@@ -17,7 +15,7 @@ public class PdfTest {
         String pdfFilePath = "./src/test/resources/files/1.pdf";
         String expectedData = "СКЪРО";
 
-        PDF pdf = new PDF(getFile(pdfFilePath));
+        PDF pdf = getPdf(pdfFilePath);
         assertThat(pdf, containsText(expectedData));
     }
 }
--- src/test/java/utils/Files.java
+++ src/test/java/utils/Files.java
@@ -1,5 +1,7 @@
 package utils;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -18,4 +20,13 @@ public class Files {
     public static File getFile(String path){
         return new File(path);
     }
+
+    public static PDF getPdf (String path) throws IOException {
+        return new PDF(getFile(path));
+    }
+
+    public static XLS getXls (String path) throws IOException {
+        return new XLS(getFile(path));
+    }
+
 }
